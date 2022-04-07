b3aa23d6515a301daa12c05c125a47f2e825e450
snehasharma1111 <snehasharma9205@gmail.com>
2022-04-07 22:09:50 +0530

    prepared about section

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 AOS.init();
 
 const header=document.querySelector('header')
+let headHeight='7rem';
 
 const vh=window.innerHeight/100;
 
 header.style.transition='all 0.5s ease-in-out'
+
+
+const varOnScroll=()=>{
+    if(window.scrollY>50*vh){
+        headHeight='5rem'
+    }
+    else{
+        headHeight='7rem'
+    }
+}
+
+document.onload=()=>{
+    header.style.height=headHeight
+}
 
 const headerOnScroll=()=>{
     if(window.scrollY>50*vh){
@@ -18,5 +33,6 @@ const headerOnScroll=()=>{
 }
 
 window.addEventListener('scroll',()=>{
+    varOnScroll()
     headerOnScroll()
 })
