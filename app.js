1e7230f32a5592444d8fa87e792b7207a8c34ce5
snehasharma1111 <snehasharma9205@gmail.com>
2022-04-07 17:18:45 +0530

    fixed header and scroll

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 AOS.init();
+
+const header=document.querySelector('header')
+
+const vh=window.innerHeight/100;
+
+header.style.transition='all 0.5s ease-in-out'
+
+const headerOnScroll=()=>{
+    if(window.scrollY>50*vh){
+        header.style.backgroundColor='var(--black)'
+        header.style.height='5rem'
+    }
+    else{
+        header.style.backgroundColor='var(--transparent)'
+        header.style.height='7rem'
+    }
+}
+
+window.addEventListener('scroll',()=>{
+    headerOnScroll()
+})
